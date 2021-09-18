f780ef7afb3f6f80421c934e4501171d628c37b4
Daniel <danieldawit33@gmail.com>
2021-09-18 11:41:48 -0400

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Currency_Converter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Currency_Converter.dir/main.cpp.o"
+  "Currency_Converter"
+  "Currency_Converter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Currency_Converter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
